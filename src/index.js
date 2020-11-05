af4c2f68cd22d84d50f0e245b772775673a576b2
zcxiao <zcxiao@zcxiaodeMacBook-Pro.local>
2020-11-05 14:50:10 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
+import { createStore } from 'redux'
+const ADD = 'add'
+const DELATE = 'delate'
+let initState = 0
+function reducer(state = initState, action) {
+  switch (action.type) {
+    case ADD:
+      return state + 1
+    case DELATE:
+      return state - 1
+
+    default:
+      return state
+
+
+  }
+}
+
+let store = createStore(reducer)
+let state = store.getState()
+console.log('state', state)
+
+let valuedom = document.getElementById('counter-value')
+let counterdom = document.getElementById('counter')
+
+function render(){
+   valuedom.innerHTML = store.getState()
+}
+store.subscribe(render)
+counterdom.addEventListener('click',function(e){
+  if(e.target.innerHTML === '+'){
+    store.dispatch({
+      type:ADD
+    })
+  }else{
+    store.dispatch({
+      type:DELATE
+    })
+  }
+  
+})
